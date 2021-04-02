fce16c3dcf8191003ca23a2e1b18ef7cb15ef90b
Delwar Ahmed <delwar.habib@gmail.com>
2021-04-02 19:17:12 +0600

    order api created

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 require('dotenv').config();
 
 const MongoClient = require('mongodb').MongoClient;
+const { ObjectID } = require('mongodb').ObjectID;
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7ihro.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
 const port = process.env.PORT || 5055;
@@ -36,8 +37,8 @@ client.connect(err => {
         })
     })
 
-    app.get('/product', (req, res) => {
-        productCollection.find({ _id: req.query._id})
+    app.get('/product/:id', (req, res) => {
+        productCollection.find({ _id: ObjectID(req.params.id) })
         .toArray( (err, product) => {
             res.send(product);
         })
